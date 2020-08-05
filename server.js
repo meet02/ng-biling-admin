7b434f42b8bf2eb3bf250767e96274fe119bdf97
meet02 <47363880+meet02@users.noreply.github.com>
2020-08-06 00:27:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ conole.log("listing")
 // app.use(express.static("dist"));
 // Serve only the static files form the dist directory
 
-app.use(express.static(path.join(__dirname + "dist")));
+app.use(express.static(path.join(__dirname,"dist")));
 
 // app.get("/*", function (req, res) {
 //   res.sendFile(path.join(__dirname + "/dist/index.html"));
