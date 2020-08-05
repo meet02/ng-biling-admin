d79481c65a1ea72547004067ab2a9dcc5c79de2c
meet02 <47363880+meet02@users.noreply.github.com>
2020-08-05 23:26:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("dist"));
+// app.use(express.static("dist"));
 // Serve only the static files form the dist directory
-console.log("dist",__dirname)
+
 app.use(express.static(__dirname + "/dist));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/dist/index.html"));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
